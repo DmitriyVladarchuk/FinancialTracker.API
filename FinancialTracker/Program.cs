f63b92f62eea4ee15f483cdd98d934241a9d0810
DmitriyVladarchuk <dimadf678@gmail.com>
2025-09-19 12:52:14 +0300

    feat: add swagger

--- FinancialTracker/Program.cs
+++ FinancialTracker/Program.cs
@@ -3,6 +3,7 @@ using FinancialTracker.Data.Repositories;
 using FinancialTracker.Interfaces;
 using FinancialTracker.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,12 +16,33 @@ builder.Services.AddScoped<ITransactionService, TransactionService>();
 builder.Services.AddDbContext<FinancialDbContext>(options => 
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Financial Tracker API",
+        Version = "v1",
+        Description = "API для управления личными финансами",
+    });
+});
+
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<FinancialDbContext>();
     dbContext.Database.Migrate();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Financial Tracker API v1");
+        c.RoutePrefix = "swagger";
+    });
 }
 
 app.UseRouting();
